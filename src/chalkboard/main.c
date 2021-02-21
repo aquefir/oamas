274a2f0c42e5ad1435e056fca9eb53fb1045a7c5
Alexander Nicholi <me@nicholatian.com>
2021-02-20 21:24:46 -0500

    add test to check framebuffer size

--- src/chalkboard/main.c
+++ src/chalkboard/main.c
@@ -4,3 +4,20 @@
  *                         Copyright © 2020 Aquefir                         *
  *                       Released under BSD-2-Clause.                       *
 \****************************************************************************/
+
+int main( int ac, char* av[] )
+{
+	FILE* f;
+	long sz;
+
+	f = fopen( "/dev/fb0", "r" );
+
+	fseek( f, 0L, SEEK_END );
+	sz = ftell( f );
+
+	fclose( f );
+
+	printf( "fb0 size: %ul bytes\n", sz );
+
+	return 0;
+}
