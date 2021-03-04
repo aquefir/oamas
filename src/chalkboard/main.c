68cb227eb852be7bb892d7033c9650959cc5b35e
Alexander Nicholi <me@nicholatian.com>
2021-03-04 17:47:19 -0500

    block out /dev/fb0 code for now

--- src/chalkboard/main.c
+++ src/chalkboard/main.c
@@ -5,19 +5,21 @@
  *                       Released under BSD-2-Clause.                       *
 \****************************************************************************/
 
-int main( int ac, char* av[] )
+int main( int ac, char * av[] )
 {
-	FILE* f;
+	FILE * f;
 	long i;
 	int r;
 
+#if 0
 	f = fopen( "/dev/fb0", "r+" );
-	for( i = 0, r = 0; r != EOF; ++i)
+	for( i = 0, r = 0; r != EOF; ++i )
 	{
 		r = fputc( 0, f );
 	}
 
 	printf( "i == %lu\n", i );
+#endif /* 0 */
 
 	return 0;
 }
