cfe7c464077e2f9034a51199b5274bb3c55345a6
Alexander Nicholi <me@nicholatian.com>
2021-03-04 22:22:14 -0500

    remove cosmo fetchers, it’s part of Slick now

--- util/rmcosmo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# -*- coding: utf-8 -*-
-
-echo -n 'Removing the Cosmopolitan downloads and binaries... ' >/dev/stderr;
-rm -f etc/cosmo.zip;
-rm -rf etc/cosmo;
-echo 'done.' >/dev/stderr;
-
-exit 0;
